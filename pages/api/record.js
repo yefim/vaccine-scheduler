5ee79d485190eb360ec474bbaf43bd23bddb29ac
Yefim Vedernikoff <hi@yef.im>
2021-02-04 21:32:48 -0700

    Specify more things

--- pages/api/record.js
+++ pages/api/record.js
@@ -4,11 +4,15 @@ export default (req, res) => {
   const twiml = new VoiceResponse();
 
   twiml.say({voice: 'alice'}, 'Please leave your name and phone number at the beep.');
+  // https://www.twilio.com/docs/voice/twiml/record
   twiml.record({
+    timeout: 0, // disable ending the record after X seconds of silence
     transcribe: true,
     transcribeCallback: '/api/schedule',
-    maxLength: 60
+    maxLength: 60, // seconds
+    playBeep: true,
   });
+  twiml.hangup();
 
   res.setHeader('Content-Type', 'text/xml');
   res.write(twiml.toString());
